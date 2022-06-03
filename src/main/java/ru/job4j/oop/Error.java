540235fc22e7b15d7f2bfdf544eba282a675a803
NickUli <xlloid@mail.ru>
2022-06-03 21:43:28 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public static void main(String[] args) {
+        Error processNull = new Error();
+        Error processInterrupted = new Error(false, 130, "Interrupted process");
+        Error processError = new Error(true, 1, "ArrayIndexOutOfBoundsException");
+        processNull.printInfo();
+        processInterrupted.printInfo();
+        processError.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Exception: " + this.active);
+        System.out.println("Process finished with exit code " + this.status);
+        System.out.println("Info: " + this.message + "\n");
+    }
+}
