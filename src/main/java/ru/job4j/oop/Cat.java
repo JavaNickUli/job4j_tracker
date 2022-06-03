9e493f9be1d799420b37355829cb2b312769bcc3
NickUli <xlloid@mail.ru>
2022-06-03 20:00:53 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("There are Gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        System.out.println("There are Black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eats a " + this.food + ".");
+    }
+}
