21f5322195daddeb5bc73475e829483e14d3a758
NickUli <xlloid@mail.ru>
2022-06-08 12:36:35 +0300

    3. Интерфейс Input [#181092 #310153]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,29 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private int freePlace = 100;
+
     @Override
     public void drive() {
-
+        System.out.println("Автобус отправляется в пункт назначения");
     }
 
     @Override
     public void passengers(int quantity) {
-
+        if (freePlace >= quantity) {
+            freePlace -= quantity;
+            System.out.println("В автобусе " + (100 - freePlace) + " пассажир(а/ов)");
+        } else {
+            quantity -= freePlace;
+            System.out.println("Автобус полный. " + quantity + " человек(а) не поместились.");
+        }
     }
 
     @Override
     public double refuel(double fuel) {
-        return 0;
+        double cost = fuel * 49.54;
+        System.out.println("Автобус запрвлен на " + fuel + " литр(а/ов).\n"
+                + " Выдан чек на сумму: " + cost + "руб.");
+        return cost;
     }
 }
