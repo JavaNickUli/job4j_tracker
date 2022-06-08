7561a77d533faca91648d39a1f9bab55904406cf
NickUli <xlloid@mail.ru>
2022-06-08 14:03:32 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,12 +7,34 @@ public class StartUITest {
 
     @Test
     public void whenCreateItem() {
-        String[] answers = {"Test"};
+        String[] answers = {"Create test"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
-        Item expected = new Item("Test");
+        Item expected = new Item("Create test");
         Assert.assertEquals(created.getName(), expected.getName());
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Create test for edit");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "Edited test"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        Assert.assertEquals(edited.getName(), "Edited test");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Create test for delete");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Assert.assertNull(deleted);
     }
 }
