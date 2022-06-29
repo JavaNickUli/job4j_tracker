e264b65f30c98271bccfdc6b442600dc94c09125
NickUli <xlloid@mail.ru>
2022-06-29 13:23:20 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,7 +6,6 @@ public class DepDescComp implements Comparator<String> {
 
     @Override
     public int compare(String string1, String string2) {
-
         int result = string2.split("/")[0].compareTo(string1.split("/")[0]);
         return result == 0 ? string1.compareTo(string2) : result;
     }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -3,19 +3,15 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class Departments {
-
     public static List<String> fillGaps(List<String> departments) {
         Set<String> result = new LinkedHashSet<>();
-
         for (String department : departments) {
             String assembly = "";
-
             for (String element : department.split("/")) {
                 assembly += (assembly.isEmpty() ? "" : "/") + element;
                 result.add(assembly);
             }
         }
-
         return new ArrayList<>(result);
     }
 
